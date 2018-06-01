961b9a9d6fadd6258a7ebb2ac6374f3545c2570e
Adam Tolaini <adam.tolaini@gmail.com>
2018-06-01 15:03:21 +0100

    welcome section elements in postion

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-//Header parallax
 window.addEventListener('scroll', () => {
     let wScroll = this.pageYOffset;
-    let logo = document.querySelector('.siteHeader__logo');
 
+    //HEADER PARALLAX
+    let logo = document.querySelector('.siteHeader__logo');
     logo.style.transform = 'translate(0, '+ wScroll / 3 + '%)';
+
+    //WELCOME PARALLAX
+    let welcomeImge = document.querySelector('.welcome__image');
+    
 });
