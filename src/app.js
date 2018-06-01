65e1cdc68a48483f33d8e72b93bc8bfe1c7f9c8c
Adam Tolaini <adam.tolaini@gmail.com>
2018-06-01 15:51:50 +0100

    welcome section functionality working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ window.addEventListener('scroll', () => {
     logo.style.transform = 'translate(0, '+ wScroll / 3 + '%)';
 
     //WELCOME PARALLAX
-    let welcomeImge = document.querySelector('.welcome__image');
-    
+    let welcome = document.querySelector('.welcome');
+    let welcomeImg = document.querySelector('.welcome__image');
+
+    if(wScroll > welcome.offsetTop - (window.innerHeight / 2)) {
+        welcomeImg.style.transform = 'translate(0,' + wScroll / -25 + '%)';
+    }
 });
