729023316400e49dd36c59b7f5eb3bd4d22e768a
Adam Tolaini <adam.tolaini@gmail.com>
2018-06-05 21:46:11 +0100

    response started

--- src/app.js
+++ src/app.js
@@ -100,7 +100,35 @@ $(document).ready(function(){
         variableWidth: true,
         centerMode: true,
         centerPadding: '60px',
-        autoplay: true
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     });
   });
 
