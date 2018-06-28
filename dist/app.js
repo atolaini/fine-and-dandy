1cd9d84d107ad9f5f3b7cac370b75b4091080390
Adam Tolaini <adam.tolaini@gmail.com>
2018-06-28 15:29:33 +0100

    updates finished

---
+++ dist/app.js
@@ -0,0 +1,135 @@
+//NAVIGATION    
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  const navigation = document.querySelector('.navigation')
+  const navCheckbox = navigation.querySelector('.navigation__checkbox')
+  const navItems = navigation.querySelectorAll('.navigation__list--item')
+  navItems.forEach(item =>
+      item.addEventListener('click', _ => navCheckbox.checked = false))
+
+
+
+//PARALLAX FUNCTIONALITY..............................
+
+window.addEventListener('scroll', () => {
+    let wScroll = this.pageYOffset;
+
+    //HEADER PARALLAX
+    let logo = document.querySelector('.siteHeader__logo');
+    logo.style.transform = 'translate(0, '+ wScroll / 3 + '%)';
+
+    //WELCOME PARALLAX
+    let welcome = document.querySelector('.welcome');
+    let welcomeImg = document.querySelector('.welcome__image');
+
+    if(wScroll > welcome.offsetTop - (window.innerHeight / 1)) {
+        welcomeImg.style.transform = 'translate(0px,' + wScroll / -20 + '%)';
+    }
+    
+});
+
+
+//GALLERY MODAL AND SLIDER FUNCTIONALITY...............
+
+let myImages = document.querySelectorAll('.gallery__img');
+let modal = document.querySelector('.galleryModal');
+let closeBtn = document.querySelector('.closeBtn');
+let current = document.querySelector('.current');
+let next = document.querySelector('.nextBtn');
+let prev = document.querySelector('.prevBtn');
+let newSrc = [];
+
+//OPEN MODAL ON IMAGE CLICK
+myImages.forEach(img => {
+    img.addEventListener('click', (e) => {
+        modal.style.display = 'block';
+        current.src = e.target.src;
+        console.log(current.src)
+    })
+})
+
+//CLOSE MODAL
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
+
+
+//TESTIMONIALS SLIDER
+$(document).ready(function(){
+    $('.testimonials__cardsWrap').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        centerMode: true,
+        centerPadding: '60px',
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+  });
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,47 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const prefix = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const prefix = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const cleanCSS = require("gulp-clean-css");
 
 //TASKS
 
 //Copy HTML
-gulp.task('copyHtml', () => {
-    gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'))
+gulp.task("copyHtml", () => {
+  gulp.src("src/*.html").pipe(gulp.dest("dist"));
 });
 
 //Compile SASS and prefix CSS
-gulp.task('sass', () => {
-    gulp.src(['src/scss/**/*.scss'], ['sass'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(prefix('last 2 versions'))
-        .pipe(gulp.dest('dist/css'))
+gulp.task("sass", () => {
+  gulp
+    .src(["src/scss/**/*.scss"], ["sass"])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(prefix("last 2 versions"))
+    .pipe(gulp.dest("dist/css"));
 });
 
 //Image Minify
-gulp.task('minify', () => {
-    gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+gulp.task("minify", () => {
+  gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
+});
+
+//CSS MINIFY
+gulp.task("minify-css", () => {
+  return gulp
+    .src("dist/css/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("dist"));
 });
 
 //Wacth task
-gulp.task('serve', ['sass', 'copyHtml', 'minify'], () => {
-    gulp.watch(['src/scss/**/*.scss'],['sass'])
-    gulp.watch(['src/*.html'], ['copyHtml'])
-    gulp.watch(['src/images/*'], ['minify'])
+gulp.task("serve", ["sass", "copyHtml", "minify", "minify-css"], () => {
+  gulp.watch(["src/scss/**/*.scss"], ["sass"]);
+  gulp.watch(["src/*.html"], ["copyHtml"]);
+  gulp.watch(["src/images/*"], ["minify"]);
 });
 
 //Default Task
-gulp.task('default', ['serve']);
+gulp.task("default", ["serve"]);
